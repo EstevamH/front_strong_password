67b8cd7c7f2f48c97882d8ee0e2a46e83a1a7576
estevam.rodrigues <estevam.rodrigues@itbam.org.br>
2021-04-14 18:30:15 -0400

    feat: add user login form validator

---
+++ src/app/domain/usecases/user/validation/user-login-form-validator.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+import { ValidationError } from '../../../interfaces/validation/validation.error';
+import { ValidationComposite } from '../../../../validation/validation-composite/validation-composite';
+import { ValidationBuilder } from '../../../../validation/builder/validation-builder';
+import { UserEntity } from '../../../entities/user-entity';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginFormValidatorService {
+  private _errors: ValidationError[] = [];
+  private _form: FormGroup;
+
+  constructor() {}
+
+  isValidFields(param: UserEntity): ValidationError[] {
+    Object.keys(param).forEach((item) => {
+      let error = this.rules().validate(item, param[item]);
+      if (error) {
+        this._errors.push(error);
+      }
+    });
+    this.setFormValidation(this._errors);
+    return this._errors;
+  }
+
+  private setFormValidation(errors: ValidationError[]): void {
+    Object.keys(this._form.controls).forEach((field) => {
+      let error = errors.filter((e) => e.field === field)[0];
+      if (error) {
+        let control = this._form.get(field);
+        let validation = error.errorName;
+        control.setErrors({ [validation]: true });
+      }
+    });
+  }
+
+  setForm(form: FormGroup): void {
+    this._form = form;
+  }
+
+  private rules(): ValidationComposite {
+    return ValidationComposite.build([
+      ...ValidationBuilder.field('email').required().email().build(),
+      ...ValidationBuilder.field('password').required().build(),
+    ]);
+  }
+}
--- src/app/validation/field-error-message.service.ts
+++ src/app/validation/field-error-message.service.ts
@@ -1,13 +1,15 @@
+import { InvalidFieldError } from './errors/invalid-field-error';
 import { Injectable } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
+import { RequiredFieldError } from './errors/required-field-error';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FieldErrorMessage {
   private errorMessage = {
-    required: (params) => `Campo Obrigatório`,
-    email: (params) => `Email inválido`,
+    required: (params) => new RequiredFieldError().message,
+    email: (params) => new InvalidFieldError().message,
   };
 
   message: string;
