cd1ca09da5c8d5f2b12fc4a8bee3416afd13b52e
estevam.rodrigues <estevam.rodrigues@itbam.org.br>
2021-04-19 08:38:23 -0400

    Revert "bad commits"
    This reverts commit cf2adf37d612aec833647eeb80996e0e41dc5317.

--- src/app/domain/entities/base/domain-entity.ts
+++
@@ -1,3 +0,0 @@
-export class DomainEntity {
-  id?: number = null;
-}
--- src/app/domain/entities/user-entity.ts
+++
@@ -1,6 +0,0 @@
-import { DomainEntity } from './base/domain-entity';
-export class UserEntity extends DomainEntity {
-  name?: string = null;
-  password?: string = null;
-  email?: string = null;
-}
